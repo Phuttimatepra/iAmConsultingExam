526b5cf555a88f48cba11cc708ab43e6a54379ac
phuttimate <phuttimate@netcube.co.th>
2019-12-23 13:03:42 +0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'PalindromeOrNot';
 }
+
+// !  No.2 ===================================================
+
+// ! ======================= Javascript =================
+
+let word = "deleveled";
+console.log(word, isPalindrome(word));
+function isPalindrome(str) {
+  let mid = Math.floor(str.length / 2);
+  let round = 0, temp = 0;
+  for (let i = 0; i <= mid; i++) {
+    round++;
+    if (str[i] != str[str.length - i - 1]) {
+      temp--;
+    }
+    temp++;
+  }
+  if (temp == round)
+    return true;
+  else
+    return false;
+}
